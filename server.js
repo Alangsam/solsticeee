bbda81125b2a4dca154a05d1dd08d5e87f9f6f39
Alex <alangsam98@gmail.com>
2020-08-18 18:13:26 -0700

    .

--- server.js
+++ server.js
@@ -6,10 +6,6 @@ const path = "initial.json";
 
 app.use("/api/v1", express.static("api/v1"));
 
-app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-});
-
 const port = process.env.PORT || 3013;
 app.listen(port, () =>
     console.log(`Server running at http://localhost:${port}`)
