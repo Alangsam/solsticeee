efb91782bfb6aa3d4a3608a2416a6d98413a1274
Alex <alangsam98@gmail.com>
2020-08-18 19:32:14 -0700

    .

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const app = express();
-const path = "initial.json";
+const path = require("path");
 
 // app.use(express.static("/api/v1"));
 //track my shit please
-app.use("/api/v1", express.static("api/v1"));
+//app.use("/api/v1", express.static("api/v1"));
 
 // app.get("/", function (req, res) {
 //     res.render("index", {});
 // });
+app.use(express.static("client/build"));
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
